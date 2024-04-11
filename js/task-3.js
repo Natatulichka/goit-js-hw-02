9bad8c99683193234072020642b6f12a44fa0f1f
Natalia Huliaieva <155992841+Natatulichka@users.noreply.github.com>
2024-04-11 17:13:02 +0200

    hw-2

--- js/task-3.js
+++ js/task-3.js
@@ -1,14 +1,16 @@
-// Ширина елемента
-function getElementWidth(content, padding, border) {
-  const value = content;
-  const value1 = padding;
-  const value2 = border;
-  const numerical =
-    Number.parseFloat(value) +
-    Number.parseFloat(value1) * 2 +
-    Number.parseFloat(value2) * 2;
-  return numerical;
+// Перевірка спаму
+function checkForSpam(message) {
+  const lowerCaseMessage = message.toLowerCase();
+  if (lowerCaseMessage.includes("spam") || lowerCaseMessage.includes("sale")) {
+    return true;
+  } else {
+    return false;
+  }
 }
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
